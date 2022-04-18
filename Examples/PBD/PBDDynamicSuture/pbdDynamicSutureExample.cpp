804b0ba23c59481b56700f9e723d37578b139e84
Jacob Moore <jacob.moore@kitware.com>
2022-04-18 08:53:19 -0500

    ENH: Simplifying to unit test

--- Examples/PBD/PBDDynamicSuture/pbdDynamicSutureExample.cpp
+++ Examples/PBD/PBDDynamicSuture/pbdDynamicSutureExample.cpp
@@ -73,44 +73,51 @@ struct Input
 
 Input input;
 
-// Create tissue object to stitch
-std::shared_ptr<PbdObject> 
-createTissueHole(std::shared_ptr<TetrahedralMesh> tetMesh)
+std::shared_ptr<SurfaceMesh>
+createTriangle()
 {
 
-	std::shared_ptr<SurfaceMesh> boundMesh = tetMesh->extractSurfaceMesh();
+	auto triangleMesh = std::make_shared<SurfaceMesh>();
 
-	auto pbdObject = std::make_shared<PbdObject>("meshHole");
+	VecDataArray<double, 3> triangleVertices(3);
+    triangleVertices[0] = Vec3d(-0.1, 0.0, 0.0);
+    triangleVertices[1] = Vec3d(0.0, 0.0, 0.1);
+    triangleVertices[2] = Vec3d(0.1, 0.0, 0.0);
+    VecDataArray<int, 3> triangleIndices(1);
+    triangleIndices[0] = Vec3i(0, 1, 2);
+    triangleMesh->initialize(
+        std::make_shared<VecDataArray<double, 3>>(triangleVertices),
+        std::make_shared<VecDataArray<int, 3>>(triangleIndices));
+
+    return triangleMesh;
+
+
+}
+
+// Create tissue object to stitch
+static std::shared_ptr<PbdObject> 
+createPbdTriangle()
+{
+
+
+	std::shared_ptr<SurfaceMesh> triMesh = createTriangle();
+
+	auto pbdObject = std::make_shared<PbdObject>("pbdTriangle");
 	auto pbdParams = std::make_shared<PbdModelConfig>();
 	
 	pbdParams->enableConstraint(PbdModelConfig::ConstraintGenType::Distance, 10.0);
-	pbdParams->enableConstraint(PbdModelConfig::ConstraintGenType::Volume, 10.0);
+	pbdParams->enableConstraint(PbdModelConfig::ConstraintGenType::Dihedral, 10.0);
 	pbdParams->m_doPartitioning   = false;
-	pbdParams->m_uniformMassValue = 0.01;
-	pbdParams->m_gravity    = Vec3d(0.0, 0.0, -0.01);
+	pbdParams->m_uniformMassValue = 0.1;
+	pbdParams->m_gravity    = Vec3d(0.0, 0.0, 0.0);
 	pbdParams->m_dt = 0.01;
 	pbdParams->m_iterations = 5;
 	pbdParams->m_viscousDampingCoeff = 0.01;
 
-	// Fix the borders
-	for (int vert_id = 0; vert_id < boundMesh->getNumVertices(); vert_id++)
-	{
-		auto position = tetMesh->getVertexPosition(vert_id);
-		if (std::fabs(1.40984-std::fabs(position(1))) <= 1E-4)
-		{
-			pbdParams->m_fixedNodeIds.push_back(vert_id);
-		}
-	}
-
-	tetMesh->scale(0.02, Geometry::TransformType::ApplyToData);
-	tetMesh->rotate(Vec3d(0.0, 0.0, 1.0), -PI_2, Geometry::TransformType::ApplyToData);
-	tetMesh->rotate(Vec3d(1.0, 0.0, 0.0), -PI_2/6.0, Geometry::TransformType::ApplyToData);
-
-	std::shared_ptr<SurfaceMesh> surfMesh = tetMesh->extractSurfaceMesh();
 
 	// Setup the Model
 	auto pbdModel = std::make_shared<PbdModel>();
-	pbdModel->setModelGeometry(tetMesh);
+	pbdModel->setModelGeometry(triMesh);
 	pbdModel->configure(pbdParams);
 
 	// Setup the material
@@ -119,39 +126,21 @@ createTissueHole(std::shared_ptr<TetrahedralMesh> tetMesh)
 
 	// Add a visual model to render the surface of the tet mesh
 	auto visualModel = std::make_shared<VisualModel>();
-	visualModel->setGeometry(tetMesh);
+	visualModel->setGeometry(triMesh);
 	visualModel->setRenderMaterial(material);
 	pbdObject->addVisualModel(visualModel);
 
 	// Setup the Object
-	pbdObject->setPhysicsGeometry(tetMesh);
-	pbdObject->setCollidingGeometry(surfMesh);
-	pbdObject->setPhysicsToCollidingMap(std::make_shared<PointwiseMap>(tetMesh, surfMesh));
+	pbdObject->setPhysicsGeometry(triMesh);
+	pbdObject->setCollidingGeometry(triMesh);
 	pbdObject->setDynamicalModel(pbdModel);
 
 	return pbdObject;
 }
 
 
-static std::shared_ptr<SceneObject>
-makeClampObj(std::string name)
-{
-	auto surfMesh =
-		MeshIO::read<SurfaceMesh>(iMSTK_DATA_ROOT "/Surgical Instruments/Clamps/Gregory Suture Clamp/gregory_suture_clamp.obj");
 
-	surfMesh->scale(0.2, Geometry::TransformType::ApplyToData);
 
-	auto toolObj = std::make_shared<SceneObject>(name);
-	toolObj->setVisualGeometry(surfMesh);
-	auto renderMaterial = std::make_shared<RenderMaterial>();
-	renderMaterial->setColor(Color::LightGray);
-	renderMaterial->setShadingModel(RenderMaterial::ShadingModel::PBR);
-	renderMaterial->setRoughness(0.5);
-	renderMaterial->setMetalness(1.0);
-	toolObj->getVisualModel(0)->setRenderMaterial(renderMaterial);
-
-	return toolObj;
-}
 
 
 ///
@@ -163,53 +152,33 @@ main()
 	// Setup logger (write to file and stdout)
 	Logger::startLogger();
 
-	input.meshFileName = iMSTK_DATA_ROOT "Tissues/tissue_hole.vtk";
+	// input.meshFileName = iMSTK_DATA_ROOT "Tissues/tissue_hole.vtk";
 
 	// Construct the scene
 	auto scene = std::make_shared<Scene>("DynamicSuture");
 	
-	scene->getActiveCamera()->setPosition(0.0, 0.0, 0.15);
-	scene->getActiveCamera()->setFocalPoint(0.0, 0.0, -0.02);
-	scene->getActiveCamera()->setViewUp(0.0, 1.0, 0.0);
+	scene->getActiveCamera()->setPosition(0.0, 0.06, 0.24);
+	scene->getActiveCamera()->setFocalPoint(0.0, 0.005, 0.05);
+	scene->getActiveCamera()->setViewUp(-0.01, 1.0, -0.3);
 
 	auto light = std::make_shared<DirectionalLight>();
 	light->setFocalPoint(Vec3d(5.0, -8.0, -5.0));
 	light->setIntensity(1.0);
 	scene->addLight("Light", light);
 
-	// Load a tetrahedral mesh
-	std::shared_ptr<TetrahedralMesh> tetMesh = MeshIO::read<TetrahedralMesh>(input.meshFileName);
-	CHECK(tetMesh != nullptr) << "Could not read mesh from file.";
 
-	// Mesh with hole for suturing
-	std::shared_ptr<PbdObject> tissueHole = createTissueHole(tetMesh);
-	scene->addSceneObject(tissueHole);
+	// Make Triangle
+	auto pbdTriangle = createPbdTriangle();
+	scene->addSceneObject(pbdTriangle);
+
 
 	// Create arced needle
 	auto needleObj = std::make_shared<NeedleObject>();
 	scene->addSceneObject(needleObj);
 
 	// Add needle constraining behaviour between the tissue & arc needle
-	auto needleInteraction = std::make_shared<NeedleInteraction>(tissueHole, needleObj);
-	scene->addInteraction(needleInteraction);
-
-
-
-
-
-
-
-	// Create clamps that follow the needle around
-	std::shared_ptr<SceneObject> toolObj = makeClampObj("Clamps");
-	scene->addSceneObject(toolObj);
-
-	// Create ghost clamps to show real position of hand under virtual coupling
-	std::shared_ptr<SceneObject> ghostToolObj = makeClampObj("GhostClamps");
-	ghostToolObj->getVisualModel(0)->getRenderMaterial()->setColor(Color::Orange);
-	scene->addSceneObject(ghostToolObj);
-
-
-	// Create the suture pbd-based string
+	// auto needleInteraction = std::make_shared<NeedleInteraction>(tissueHole, needleObj);
+	// scene->addInteraction(needleInteraction);
 
 
 	// Run the simulation
@@ -253,33 +222,9 @@ main()
 			[&](Event*)
 			{
 				needleObj->getRigidBodyModel2()->getConfig()->m_dt = sceneManager->getDt();
+				// pbdTriangle->getPbdModel()->getConfig()->m_dt = sceneManager->getDt();
 			});
 		
-
-		// Transform the clamps relative to the needle
-		const Vec3d clampOffset = Vec3d(-0.009, 0.01, 0.001);
-		connect<Event>(sceneManager, &SceneManager::postUpdate,
-			[&](Event*)
-			{
-				toolObj->getVisualGeometry()->setTransform(
-					needleObj->getVisualGeometry()->getTransform() *
-					mat4dTranslate(clampOffset) *
-					mat4dRotation(Rotd(PI, Vec3d(0.0, 1.0, 0.0))));
-				toolObj->getVisualGeometry()->postModified();
-			});
-		
-		// Transform the ghost tool clamps to show the real tool location
-		connect<Event>(sceneManager, &SceneManager::postUpdate,
-			[&](Event*)
-			{
-				ghostToolObj->getVisualGeometry()->setTransform(
-					mat4dTranslate(hapController->getPosition()) * mat4dRotation(hapController->getOrientation()) *
-					mat4dTranslate(clampOffset) *
-					mat4dRotation(Rotd(PI, Vec3d(0.0, 1.0, 0.0))));
-				ghostToolObj->getVisualGeometry()->updatePostTransformData();
-				ghostToolObj->getVisualGeometry()->postModified();
-				ghostToolObj->getVisualModel(0)->getRenderMaterial()->setOpacity(std::min(1.0, hapController->getDeviceForce().norm() / 5.0));
-			});
 
 
 		// Add mouse and keyboard controls to the viewer
